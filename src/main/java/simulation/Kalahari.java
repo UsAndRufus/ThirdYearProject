4bbe88d953752808917d94d139766e218519f080
Edward <pe.ads0@gmail.com>
2017-02-17 10:30:11 +0000

    simulation progress bar and small changes

--- src/main/java/simulation/Kalahari.java
+++ src/main/java/simulation/Kalahari.java
@@ -26,6 +26,7 @@ public class Kalahari {
 
     private double fractionOfCellsToUpdateEveryTick = 0.2;
     private double immediacyFactor = 3.0;
+    private double years = 200;
 
     public Kalahari(int numberOfRows, int numberOfColumns, double proportionVegetation) {
         CellFactory kalahariCellFactory = new KalahariCellFactory(proportionVegetation);
@@ -37,12 +38,20 @@ public class Kalahari {
     }
 
     public void run() {
-        grid.printToConsole();
-        for (int year = 0; year < 200; year++) {
-            System.out.println("Tick: " + year);
-            tick();
+
+        System.out.println("Starting simulation");
+
+
+        for (int year = 0; year < years; year ++) {
+            System.out.print("X");
         }
-        grid.printToConsole();
+        System.out.println();
+
+        for (int year = 0; year < years; year++) {
+            tick();
+            System.out.print("X");
+        }
+        System.out.println();
 
         KalahariClusteringMetric kalahariClusteringMetric = new KalahariClusteringMetric(grid);
 
@@ -56,7 +65,6 @@ public class Kalahari {
     }
 
     private void tick() {
-        long timeOfLastPrint = System.currentTimeMillis();
         List<Position> positions = grid.getRandomPositions(fractionOfCellsToUpdateEveryTick);
 
         for (Position position : positions) {
